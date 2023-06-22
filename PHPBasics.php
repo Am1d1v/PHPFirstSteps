a5da33069de920333aa1aaf099f2e3f46de1da63
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-22 13:43:16 +0300

    Add files via upload

--- PHPBasics.php
+++ PHPBasics.php
@@ -3,4 +3,14 @@
  
  <?php 
     echo "Hello";
+
+    $name = "</br>Dima";
+    $user = "</br> <h1>Dima</h1>";
+    echo $name;
+    echo $user;
+    echo "My name is $name </br>";
+
+    $lastName = "someLastName";
+    echo "User's Lastname is $lastName";
+
         ?>
