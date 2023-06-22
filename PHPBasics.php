1d5e13d12f5f3988f039ab8fe258c1aeef8de9dd
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-22 21:24:26 +0300

    Add files via upload

--- PHPBasics.php
+++ PHPBasics.php
@@ -35,10 +35,43 @@
     $res = $x + $y;
     echo "<p>$res<p>";
 
-    $bool = true;
-    echo "$bool";
+    //$bool = true;
+    //echo "$bool";
 
-    $variableName = "userName";
-    echo $$variableName ;
-
+   // $variableName = "userName";
+   // echo $$variableName ;
     ?>
+
+
+<?php 
+
+$colors = array("black", "pink", "white", "purple", "grey", "blue");
+
+print_r($colors);
+
+echo "<p>$colors[0]</p>";
+
+$names[0] = "Anna";
+$names[1] = "ALice";
+$names[2] = "Lea";
+$names["someName"] = "Dima";
+
+print_r($names);
+
+echo "<br>";
+echo $names["someName"];
+echo "<br>";
+
+$years["Dima"] = 2009;
+$years["Anna"] = 1999;
+$years["Alice"] = 1989;
+
+print_r($years);
+echo "<br>";
+echo sizeof($years);
+
+echo "<br>";
+
+unset($years["Dima"]);
+print_r($years);
+?>
